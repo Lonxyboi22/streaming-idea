9b8f52228d1611394207edb5aa2ad61d0cca6af9
Lonxyboi <drewthemachine22@gmail.com>
2023-07-18 20:08:08 -0400

    trying to fix the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
+import { menuItems } from "../menuItems";
+import MenuItems from "./MenuItems";
 const Navbar = () => {
     return (
       <nav>
-        <ul className="menus">Nav Items here</ul>
+        <ul className="menus">
+            {menuItems.map((menu, index) => {
+                return <MenuItems items={menu} key={index} />;
+            })}
+        </ul>
       </nav>
     );
   };
